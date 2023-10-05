e4ce9825ad38cce5d07a00674194332f7c619a9f
Ryan Shore <mshorecode@gmail.com>
2023-10-05 09:45:15 -0500

    updated styling

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,30 @@
 // import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
 // import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
 // import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 
 function Home() {
   // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
+  const [value, setValue] = useState(0);
 
-  const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
+  const handleClick = (action) => {
+    switch (action) {
+      case 'Increment':
+        setValue((prevState) => prevState + 1);
+        break;
+      case 'Decrement':
+        setValue((prevState) => prevState - 1);
+        break;
+      case 'Reset':
+        setValue(0);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const user = { displayName: 'Ryan' }; // TODO: COMMENT OUT FOR AUTH
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -16,7 +35,12 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Hello {user.displayName}! </h1>
+      <h1 id="greeting">Hello {user.displayName}! </h1>
+      <h1 id="mainText">Count Your Goblin Loot!</h1>
+      <h2 id="counterText"><span id="emoji">💰</span> {value}</h2>
+      <Button type="button" variant="danger" onClick={() => handleClick('Increment')}>Increment</Button>{' '}
+      <Button type="button" variant="danger" onClick={() => handleClick('Decrement')}>Decrement</Button>
+      <Button type="button" variant="danger" onClick={() => handleClick('Reset')}>Reset</Button>
     </div>
   );
 }
